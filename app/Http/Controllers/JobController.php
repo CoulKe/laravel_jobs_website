c0d71f0b89b26b37e010456d8db46337456bf3b7
Luteya Coulston <thatcoul@gmail.com>
2020-10-25 13:12:05 +0300

    Added single view function

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Job;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class JobController extends Controller
 {
     //
     public function index()
     {
-        $jobs = Job::all();
-
-        return view('jobs.index',[
-            'jobs' => $jobs,
+        $collection = DB::table('jobs')
+        ->join('users','user_id','=','users.id')
+        ->select('*')
+        ->get();
+        return view('jobs.index', [
+            'collection' => $collection,
         ]);
+    }
+    public function show($id){
+        $job = Job::findOrFail($id);
+        return view('jobs.show', ['job' => $job]);
     }
 }
