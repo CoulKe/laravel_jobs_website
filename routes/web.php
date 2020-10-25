6fa03fdd54fffa75871333b4670725136b9c5a40
Luteya Coulston <thatcoul@gmail.com>
2020-10-25 13:13:31 +0300

    Added single job route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/jobs',[JobController::class, 'index']);
+Route::get('jobs',[JobController::class, 'index']);
+Route::get('jobs/{id}',[JobController::class, 'show'])->where(['id'=>'[0-9]+']);
