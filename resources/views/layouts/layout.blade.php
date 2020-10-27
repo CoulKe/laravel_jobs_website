6d81c0c0da7d476fd063a38fe94cd710e9c78ae6
Luteya Coulston <thatcoul@gmail.com>
2020-10-27 13:45:15 +0300

    Changed design

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,22 +6,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/normalize.css">
     <!-- Styles -->
+    <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/main.css">
     <title>Jobs website | @yield('title')</title>
 </head>
 
 <body>
-    <nav>
-        <div class="container nav">
-            <p id="site-title"><a href="index">Job search</a></p>
-            <ul>
-                <li><a class="link" href="/">Home</a></li>
-                <li><a class="link" href="/jobs">Jobs</a></li>
-                <li><a class="link" href="/employers">Employers</a></li>
-                <li><a class="link" href="/candidates">Candidates</a></li>
-               
-            </ul>
-        </div>
+    <nav class="navbar navbar-expand-md">
+        <a class="navbar-brand" href="/">Jobs Search</a>
+        <ul class="nav collapse navbar-collapse justify-content-center">
+            <li class="nav-item">
+                <a href="/" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="/jobs" class="nav-link">Jobs</a>
+            </li>
+            <li class="nav-item">
+                <a href="/employers" class="nav-link">Employers</a>
+            </li>
+            <li class="nav-item">
+                <a href="/candidates" class="nav-link">Candidates</a>
+            </li>
+        </ul>
+        
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+                @endguest
+                @auth
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endauth
+        </ul>
     </nav>
     <main>
         @yield('content')
