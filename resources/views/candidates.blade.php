d237b10e5699f3cb391b4c524d94754efe95104b
Luteya Coulston <thatcoul@gmail.com>
2020-10-27 15:20:45 +0300

    Added new design

---
+++ resources/views/candidates.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container candidates-page">
+    <aside>
+        <form action="" method="post">
+            <label for="Skills search">Skills search:</label> <br>
+            <input type="text" name="skills_search" placeholder="type keywords"> <br>
+            <label for="Rate">Rate per hour (USD):</label> <br>
+            <input type="text" name="rate_search" placeholder="search rate"> <br>
+            <input type="submit" value="Search">
+        </form>
+    </aside>
+    <section class="candidates-section">
+        <h1 class="title">Candidates</h1>
+
+        @foreach ($candidates as $candidate)
+        <div class="candidate">
+            
+            @if (empty($candidate->profile_url))
+                <img src="" alt="{{ $candidate->name }} " class='candidate_image'>
+            @endif
+            <div class="candidate_details">
+                <div class="candidate_name">Name: {{ $candidate->first_name }}</div>
+                <div class="candidate_rate">Rate per hour: {{  $candidate->rate == 0 ? 'Negotiable' : $candidate->rate }}</div>
+                <div class="candidate_skills"> Skills: {{  $candidate->skills == '' ? 'Not listed' : $candidate->skills }} </div>
+                <a href="profile?username={{ $candidate->username }}" >View profile</a>
+            </div>
+        </div>
+        @endforeach
+    </section>
+</div>
+@endsection
---
+++ resources/views/employers.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container candidates-page">
+    <aside>
+        <form action="" method="post">
+            <label for="Skills search">Skills search:</label> <br>
+            <input type="text" name="skills_search" placeholder="type keywords"> <br>
+            <label for="Rate">Rate per hour (USD):</label> <br>
+            <input type="text" name="rate_search" placeholder="search rate"> <br>
+            <input type="submit" value="Search">
+        </form>
+    </aside>
+    <section class="candidates-section">
+        <h1 class="title">Employers</h1>
+
+        @foreach ($employers as $employer)
+        <div class="candidate">
+            
+            @if (empty($employer->profile_url))
+                <img src="" alt="{{ $employer->name }} " class='candidate_image'>
+            @endif
+            <div class="candidate_details">
+                <div class="candidate_name">Name: {{ $employer->first_name }}</div>
+                <div class="candidate_rate">Rate per hour: {{  $employer->rate == 0 ? 'Negotiable' : $employer->rate }}</div>
+                <div class="candidate_skills"> Skills: {{  $employer->skills == '' ? 'Not listed' : $employer->skills }} </div>
+                <a href="profile?username={{ $employer->username }}" >View profile</a>
+            </div>
+        </div>
+        @endforeach
+    </section>
+</div>
+@endsection
