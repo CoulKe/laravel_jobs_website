6b05fefc3c7f72a19ae77ad8fe5ea8731e3eb8d5
Luteya Coulston <thatcoul@gmail.com>
2020-10-28 18:10:08 +0300

    Fixed db method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 class ProfileController extends Controller
 {
     //
+    public $unlisted = '<p>Not listed</p>';
     public function show($username){
         $user = User::all()->whereIn('username',$username);
         return view('profile.show', ['user'=>$user]);
