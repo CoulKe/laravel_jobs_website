66cb950beefec7f41cf249973080d6db9db03b20
Luteya Coulston <thatcoul@gmail.com>
2020-10-28 21:30:52 +0300

    Added route functionality

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    //
-    public $unlisted = '<p>Not listed</p>';
-    public function show($username){
-        $user = User::all()->whereIn('username',$username);
+    /**
+     * If username is not set, find the data set using the
+     * authenticated user
+     */
+    public function show($username = ''){
+        if ($username !== '') {
+            $user = User::all()->whereIn('username',$username);
+        }
+        else{
+            $user = User::all()->whereIn('id', Auth::id());
+        }
         return view('profile.show', ['user'=>$user]);
     }
 }
