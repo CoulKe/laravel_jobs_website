094aa5c1fb97ba2ebd45a29ae065356c32917101
Luteya Coulston <thatcoul@gmail.com>
2020-10-28 18:54:02 +0300

    Designed home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,79 @@
 @extends('layouts.layout')
 @section('title','Home')
 @section('content')
-    First test
+    
+<div id="header">
+    <div id="header-wrapper">
+        <h1 id="banner-intro">Your dream job awaits you</h1>
+        <p id="slogan">just one click away</p>
+        <form action="jobs" method="get">
+            <input type="search" name="keyword" placeholder="Type keyword">
+            <input type="submit" value="Search">
+        </form>
+
+    </div>
+</div>
+
+<div class="container">
+    <section id="latest_section">
+        <h1 class="title">Latest jobs</h1>
+
+        <div class="jobs">
+            {{-- <?php foreach ($jobs_array as $jobs) : ?>
+                <div class="latest_job">
+                    <div class="image"></div>
+                    <div class="company_title">Company: <a href=<?="profile?username=".$jobs['username'];?>>
+                    <?= $jobs['company'] ?></a></div>
+                    <div class="job_description">
+                        Description:<br /> <?= $jobs['description'] ?>.
+                    </div>
+                </div>
+            <?php endforeach; ?> --}}
+        </div>
+        <a href="jobs" class="see_more">Browse more jobs</a>
+    </section>
+    <section id="post_or_join">
+        <div id="post_job">
+            <h1>Post a job</h1>
+            <a href="profile#post_job" id="post_link">Click here</a>
+        </div>
+        <div id="create_account">
+            @guest
+                <h1>Create account</h1>
+                <a href="register" id="create_link">Click here</a>
+            @endguest
+
+            @auth
+            <h1>You are logged in</h1>
+            <a href="register" id="create_link">See profile</a>
+            @endauth
+        </div>
+    </section>
+    <section id="testimonials_section">
+        <h1 class="title">Testimonials</h1>
+        <div class="carousel">
+            <img src="./assets/svg/left.svg" id="left-arr" alt="Left arrow">
+            <div class="testimony">
+                <img src="njeriredCartoon.png" alt="" class="testimony-image">
+                <div class="testimony-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa odio, minus aliquam voluptatum perferendis earum.
+                </div>
+            </div>
+            <div class="testimony">
+                <img src="mjInnocent.png" class="testimony-image" alt="testimonia">
+                <div class="testimony-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa odio, minus aliquam voluptatum perferendis earum.
+                </div>
+            </div>
+            <div class="testimony">
+                <img src="name.png" alt="" class="testimony-image">
+                <div class="testimony-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa odio, minus aliquam voluptatum perferendis earum.
+                </div>
+            </div>
+            <img src="./assets/svg/right.svg" id="right-arr" alt="Right arrow">
+        </div>
+    </section>
+</div>
+
 @endsection
