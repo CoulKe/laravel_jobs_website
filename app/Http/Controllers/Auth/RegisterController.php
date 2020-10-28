ed366b115901a1d2062d00509715847084d2ba1b
Luteya Coulston <thatcoul@gmail.com>
2020-10-28 18:06:01 +0300

    Fixed options available

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,10 +50,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255','unique:users'],
             'name' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'string', 'in:male, female'],
-            'position' => ['required', 'string', 'max:10'],
+            'position' => ['required', 'string', 'max:10','in:employer, candidate'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
