086ab62a8cb4f3bb962bc423ab2bc4e788d147b5
Luteya Coulston <thatcoul@gmail.com>
2020-10-29 12:40:32 +0300

    Edited function logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,5 +20,27 @@ class ProfileController extends Controller
             $user = User::all()->whereIn('id', Auth::id());
         }
         return view('profile.show', ['user'=>$user]);
+    }
+    public function store(){
+        $user = new User();
+
+       if (isset($_POST['post_job'])) {
+        $job = new Job();
+        $job->company = request('company_name');
+        $job->skills_required = request('skills_required');
+        $job->description = request('job_description');
+        $job->user_id = Auth::id();
+        $job->save();
+       }
+
+       if (isset($_POST['testimonial'])) {
+        //    $testimonial = new Testimonial();
+       }
+       if ($_POST['upload_pic']) {
+           $user = new User();
+           $user->profile_pic = request('profile_pic');
+           error_log($user);
+       }
+        return redirect('/profile');
     }
 }
