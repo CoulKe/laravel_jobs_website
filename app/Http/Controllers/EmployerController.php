ef271b88ac0403f59017a00ac27938330fe753b8
Luteya Coulston <thatcoul@gmail.com>
2020-11-12 11:28:38 +0300

    Added multiple key search on employers page

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -10,56 +10,41 @@ class EmployerController extends Controller
 {
     public function index(Request $request)
     {
-        $prefix = '%';
-        $suffix = '%';
-
-        if ($request) {
+        if (!isset($request->skills) && !isset($request->rate)) {
             $employers = User::where('position', 'employer')
                 ->paginate(5);
-        }
-        if (!empty($request->skills) && empty($request->rate)) {
-            
+        } else {
             $keyword = $request->skills;
             //replace non word characters with space
             $keyword = preg_replace('/\W+/', ' ', $keyword);
             //split words into array
             $keyword = preg_split('/\W/', $keyword);
             //Join the array with % OR %
-            $keyword = join('% OR %', $keyword);
-            $keyword = $prefix . $keyword . $suffix;
+            $keyword = join("%' OR `skills` LIKE '%", $keyword);
 
-            $employers = User::where('position', 'employer')
-                ->where('users.skills', 'LIKE', $keyword)
-                ->paginate(5);
-        } elseif (!empty($request->rate) && empty($request->skills)) {
+            switch ($request) {
+                case isset($request->skills) && isset($request->rate):
+                    $employers = User::where('position', 'employer')
+                        ->where('rate', $request->rate)
+                        ->whereRaw('`skills` LIKE \'%' . $keyword . '%\'')
+                        ->paginate(5);
+                    break;
+                case !isset($request->skills) && isset($request->rate):
+                    $employers = User::where('position', 'employer')
+                        ->where('rate', $request->rate)
+                        ->paginate(5);
+                    break;
+                case isset($request->skills) && !isset($request->rate):
+                    $employers = User::where('position', 'employer')
+                        ->whereRaw('`skills` LIKE \'%' . $keyword . '%\'')
+                        ->paginate(5);
+                    break;
 
-            $keyword = $request->rate;
-            // Cast rate to integer
-            $keyword = intval($keyword);
-            $keyword = $prefix . $keyword . $suffix;
-
-            $employers = User::where('position', 'employer')
-                ->where('users.rate', 'LIKE', $keyword)
-                ->paginate(5);
+                default:
+                    break;
+            }
         }
-        else {
-            $keyword = $request->skills;
-            //replace non word characters with space
-            $keyword = preg_replace('/\W+/', ' ', $keyword);
-            //split words into array
-            $keyword = preg_split('/\W/', $keyword);
-            //Join the array with % OR %
-            $keyword = join('% OR %', $keyword);
-            $keyword = $prefix . $keyword . $suffix;
-            $rate = $request->rate;
-            $rate = intval($rate);
-            $rate = $prefix . $rate . $suffix;
 
-            $employers = User::where('position', 'employer')
-                ->where('users.rate', 'LIKE', $rate)
-                ->orWhere('users.skills','LIKE',$keyword)
-                ->paginate(5);
-        }
         return view('employers', ['employers' => $employers]);
     }
 }
