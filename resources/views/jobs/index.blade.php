05787c622fd8cb564e7c10ea02a47bd8c891e5bc
Luteya Coulston <thatcoul@gmail.com>
2020-11-15 10:28:58 +0300

    Styled jobs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -6,14 +6,17 @@
     @if (count($jobs) <= 0)
     <h1 class="non-existent">Check later, there are no jobs at the moment</h1>
 @endif
+<div class="div row justify-content-between">
     @foreach ($jobs as $job)
-    <div class="job_card">
-        <p class="company_name">
-            <strong><a href={{ "profile/".$job->username }}>
-            Company:</a></strong>{{ $job->company }}</p>
-        <p class="job_description"><b>Description:</b>{{ $job->description }}</p>
-    </div>
+    
+        <div class="job_card card m-4 pl-4 pt-4 col-md-5">
+            <p class="company_name">
+                <strong><a href={{ "profile/".$job->username }}>
+                Company:</a></strong>{{ $job->company }}</p>
+            <p class="job_description"><b>Description:</b>{{ $job->description }}</p>
+        </div>
     @endforeach
+</div>
     {{ $jobs->links() }}
 </div>
 @endsection
