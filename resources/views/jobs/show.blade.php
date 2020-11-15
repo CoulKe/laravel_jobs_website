48823886c4b5f3165a5c12f51a6b9643cf5f0e96
Luteya Coulston <thatcoul@gmail.com>
2020-11-15 16:51:54 +0300

    Styled single job view

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -6,13 +6,13 @@
 @else
 @section('content')
 <div class="container">
-    <div class="job_card">
+    <div class="job_card card mx-auto mt-4 pl-4 pt-4 col-md-5">
         <p class="company_name">
             <strong><a href={{ "profile?username=".$job['name'] }}>
             Company:</a></strong>{{ $job['company'] }}</p>
         <p class="job_description"><b>Description:</b>{{ $job['description'] }}</p>
     </div>
-    <a href="/jobs">See more</a>
+    <a href="/jobs" class="text-center d-block pt-2">See more</a>
 </div>
 @endsection
 @endif
